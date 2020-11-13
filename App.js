3e67c2ecc0ec51ec788b08c2a7ceed0803d29b0c
Katie Williams <kawilliams8@gmail.com>
2020-11-12 22:12:33 -0700

    Use useState hook for basic counter and incrementor button

--- App.js
+++ App.js
@@ -1,32 +1,24 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
 
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <Text>You clicked {count} times</Text>
+      <Button style={styles.button} onPress={() => setCount(count + 1)} title="Click me!" />
+    </View>
+  );
+};
+
+// React Native Styles
 const styles = StyleSheet.create({
-  center: {
+  container: {
+    flex: 1,
+    justifyContent: "center",
     alignItems: "center",
-  },
-  name: {
-    color: 'navy',
-    fontSize: 36
   }
 });
 
-const Greeting = (props) => {
-  return (
-    <View style={styles.center}>
-      <Text style={styles.name}>Hello {props.name}!</Text>
-    </View>
-  );
-};
-
-const LotsOfGreetings = () => {
-  return (
-    <View style={[styles.center, { top: 50 }]}>
-      <Greeting name="Rexxar" />
-      <Greeting name="Jaina" />
-      <Greeting name="Valeera" />
-    </View>
-  );
-};
-
-export default LotsOfGreetings;
+export default App;
